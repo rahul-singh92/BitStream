243c633f456fb558f3db3fb96ed5029716d39cf2
Rahul Singh Jadoun <rahulsinghjadoun09@gmail.com>
2026-08-31 20:58:38 +0530

    Add message.js

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const fs = require('fs');
 const bencode = require('bencode');
 const tracker = require('./src/tracker');
 const torrentParser = require('./src/torrent-parser');
+const download = require('./src/download');
 
 // const torrent = fs.readFileSync('puppy.torrent'); // It returns a buffer, not a string
 // const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
 // console.log(torrent.announce.toString('utf8')); // This is the announce URL of the tracker i.e. Tracker's URL
 
-const torrent = torrentParser.open('puppy.torrent');
+const torrent = torrentParser.open("Marvel's Spider-Man 2 [FitGirl Repack].torrent");
 
 tracker.getPeers(torrent, peers => {
     console.log('list of peers: ', peers);
 });
+
+download(torrent);
