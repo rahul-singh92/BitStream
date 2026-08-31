1dd9908177c3cdf061a9d032091e646a84fef464
Rahul Singh Jadoun <rahulsinghjadoun09@gmail.com>
2026-08-31 21:13:46 +0530

    add onWholeMsg function

--- src/download.js
+++ src/download.js
@@ -17,7 +17,42 @@ function download(peer)
     socket.connect(peer.port, peer.ip, () => {
         // write a message here for connection
     });
+    // the socket might recieve part of message or multiple message at once. so every message start with its length to help find out the start and end of the message
+    onWholeMsg(socket, data => {
+        // handle response here
+    });
     socket.on('data', data => {
-        
+
+    });
+}
+
+// Every time the socket recieves data, the socket.on callback is called. It concats the new data
+//  with savedBuf and as long as savedBuf is long enough to contain at least one whole message, 
+// it will pass it to the onWholeMsg callback and then update savedBuf by slicing out those 
+// messages. Basically savedBuf saves the pieces of incomplete messages between rounds of 
+// receiving data from the socket.
+
+// Also the handshake is the first message we will recieve.That’s why we start with handshake 
+// set to true, and then the first time we receive a whole message we set it to false.
+
+//the onWholeMsg function is only getting called once, so the savedBuf and handshake variables 
+// are only initialized once. But then the socket.on callback gets called multiple times, each 
+// time getting and setting the same two variables.
+function onWholeMsg(socket, callback)
+{
+    let savedBuf = Buffer.alloc(0);
+    let handshake = true;
+
+    socket.on('data', recvBuffer => {
+        // msgLen calculates the length of whole message
+        const msgLen = () => handshake ? savedBuf.readUInt8(0) + 49 : savedBuf.readUInt32BE(0) + 4;
+        savedBuf = Buffer.concat([savedBuf, recvBuffer]);
+
+        while(savedBuf.length >= 4 && savedBuf.length >= msgLen())
+        {
+            callback(savedBuf.slice(0, msgLen()));
+            savedBuf = savedBuf.slice(msgLen());
+            handshake = false;
+        }
     });
 }
